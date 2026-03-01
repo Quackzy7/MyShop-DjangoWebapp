4612c3e68be466fb4a03b66bef2b1383c1009559
Quack <abhishu.panthi@gmail.com>
2026-03-01 15:53:39 +0545

    changed ui theme

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,7 +2,10 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser, BuyerProfile, SellerProfile
 
-TW = 'w-full px-3 py-2 text-sm border border-gray-300 rounded-lg outline-none focus:border-gray-500 focus:ring-1 focus:ring-gray-500 transition-colors bg-white'
+TW = 'w-full px-3 py-2 text-sm rounded-xl outline-none transition-colors' + \
+     ' border border-orange-200 bg-orange-50 text-amber-900' + \
+     ' focus:border-orange-400 focus:ring-1 focus:ring-orange-300' + \
+     ' placeholder-orange-300'
 
 
 class BuyerSignUpForm(UserCreationForm):
--- store/forms.py
+++ store/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from .models import Product
-TW = 'w-full px-3 py-2 text-sm border border-gray-300 rounded-lg outline-none focus:border-gray-500 focus:ring-1 focus:ring-gray-500 transition-colors bg-white'
+TW = 'w-full px-3 py-2 text-sm rounded-xl outline-none transition-colors' + \
+     ' border border-orange-200 bg-orange-50 text-amber-900' + \
+     ' focus:border-orange-400 focus:ring-1 focus:ring-orange-300' + \
+     ' placeholder-orange-300'
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
